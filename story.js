a5bd22d8e48ca9753b0fb7045aab128bc98573f6
d o <@>
2024-11-28 21:48:55 +0100

    add a couple of generic verb examples to show that you can return array of function or string

--- story.js
+++ story.js
@@ -7,4 +7,6 @@ story = {
 
 genericVerbMessages = {
     open: `There's nothing to open here, I'm afraid.`,
+    fly: [`You flap your arms, but nothing happens.`, `You try, but it won't work.`],
+    jump: () => {return `You jump on the spot.`}
 }
