64858edf23f8796bde748c824dfcf3a90dfdcc8b
d o <@>
2024-11-28 21:56:37 +0100

    add support for custom command handling

--- story.js
+++ story.js
@@ -1,4 +1,7 @@
 
+state = {
+
+}
 
 story = {
     openingText: "You are standing outside your apartment.# What do you want to do?",
@@ -9,4 +12,8 @@ genericVerbMessages = {
     open: `There's nothing to open here, I'm afraid.`,
     fly: [`You flap your arms, but nothing happens.`, `You try, but it won't work.`],
     jump: () => {return `You jump on the spot.`}
+}
+
+function handlePlayerCommand(c) {
+    return null
 }
--- storyDriver.js
+++ storyDriver.js
@@ -1,11 +1,19 @@
 
 
 window.doCommand = async(c) => {
+    const res0 = handlePlayerCommand(c)
+    if (res0) {
+        const text = handleResult(res0)
+        await say(text)
+        return
+    }
+    
     const res = getGenericVerbMessage(c)
     if (res) {
         await say(res)
         return
     }
+
     await displayDefaultMessage(c)
 }
 
@@ -17,13 +25,17 @@ async function displayDefaultMessage(c) {
 function getGenericVerbMessage(c) {
     const res = genericVerbMessages[c]
     if (res) {
-        if (typeof res === 'function') {
-            return res(c)
-        } else if (Array.isArray(res)) {
-            return pick(res)
-        } else {
-            return res
-        }
+        return handleResult(res)
     }
     return false
+}
+
+function handleResult(res) {
+    if (typeof res === 'function') {
+        return res(c)
+    } else if (Array.isArray(res)) {
+        return pick(res)
+    } else {
+        return res
+    }
 }
